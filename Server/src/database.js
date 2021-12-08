7994b035c05bcbe02982f3b503c86589392ce72c
Sergio Salazar <86635407+Chechox12@users.noreply.github.com>
2021-12-07 19:34:55 -0500

    password db updated

--- Server/src/database.js
+++ Server/src/database.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose')
 
-URI = ('mongodb+srv://ImTyrone:SSalazarZ121@taskmanager.ttaok.mongodb.net/TaskManager?retryWrites=true&w=majority')
+URI = ('mongodb+srv://ImTyrone:misiontic2022@taskmanager.ttaok.mongodb.net/TaskManager?retryWrites=true&w=majority')
 
 mongoose.connect(URI, {
   useNewUrlParser: true,
